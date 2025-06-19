43ec9c674ed0539579abdc35ad222d9b2472fcb9
Sobothty <supercrisz24@gmail.com>
2025-06-20 00:13:22 +0700

    ....

--- src/pages/SocialCallBack.jsx
+++ src/pages/SocialCallBack.jsx
@@ -1,18 +1,22 @@
-import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
-export const SocialCallBack = () => {
+export default function SocialCallBack() {
   const navigate = useNavigate();
+  const location = useLocation();
 
-  const email = new URLSearchParams(window.location.search).get("email");
-  fetch(`${import.meta.env.VITE_BASE_URL}/auth/social/callback`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email,
-    }),
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const email = params.get("email");
+
+    if (email) {
+      console.log("User email:", email);
+    }
+    fetch(`${import.meta.env.VITE_BASE_URL}api/auth/get-user`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email }),
+    });
   })
     .then((res) => res.json())
     .then((data) => {
@@ -29,5 +33,30 @@ export const SocialCallBack = () => {
       }
     });
 
+  // const email = new URLSearchParams(window.location.search).get("email");
+  // fetch(`${import.meta.env.VITE_BASE_URL}/auth/social/callback`, {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  //   body: JSON.stringify({
+  //     email,
+  //   }),
+  // })
+  //   .then((res) => res.json())
+  //   .then((data) => {
+  //     if (data.result) {
+  //       // Save token
+  //       localStorage.setItem("authToken", data?.data?.token);
+  //       localStorage.setItem("user", JSON.stringify(data?.data?.user));
+  //       // Redirect to home page
+  //       navigate("/");
+  //     } else {
+  //       // Handle error
+  //       console.error("Login failed:", data.message);
+  //       navigate("/login");
+  //     }
+  //   });
+
   return <div>SocialCallBack</div>;
-};
+}
